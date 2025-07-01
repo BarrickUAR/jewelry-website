5ba3117f95b1fad58aa842232b26ff175989d792
unknown <berkucar0901@gmail.com>
2025-07-01 15:52:28 +0300

    .

---
+++ app/layout.js
@@ -0,0 +1,34 @@
+// inside your layout file (e.g., layout.js or RootLayout.jsx)
+import { Karla, Cormorant_SC } from "next/font/google";
+import "./globals.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+// Fonts
+const karla = Karla({
+  subsets: ["latin"],
+  variable: "--font-karla",
+});
+
+const cormorantSC = Cormorant_SC({
+  subsets: ["latin"],
+  variable: "--font-cormorant",
+  weight: ["400", "500", "600", "700"], // Kullanacağın ağırlıklar
+});
+
+export const metadata = {
+  title: "ED Bijuteri",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={`${karla.variable} ${cormorantSC.variable}`}>
+      <body className="font-sans">
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default function Home() {
+  return (
+    <React.Fragment>
+      {/* ----------- HERO SECTION ----------- */}
+      <section>
+        <div className="heroWrapper bg-[url('/images/hero-background.png')] bg-cover h-screen flex justify-start mx-auto">
+          <div className="flex flex-col items-start justify-center text-white px-16">
+            <h1 className="text-5xl mb-8 ">the autumn equinox</h1>
+            <p className="text-2xl">Fall has arrived.</p>
+            <p className="text-2xl mb-8">
+              Shop for our new releases starting today.
+            </p>
+            <Link className="border p-5 hover:border-[#7130c2] " href="/">
+              SHOP NOW
+            </Link>
+          </div>
+        </div>
+      </section>
+      {/* ----------- HERO SECTION ----------- */}
+    </React.Fragment>
+  );
+}
